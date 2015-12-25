a1553b3ff091e2267029203479659ba1c581ad50
foxxyben <foxxyben@gmail.com>
2015-12-25 12:54:54 -0500

    Update simpliSafeAlarmState.groovy
    Include author in copyright

--- smartapps/simpliSafeAlarmState.src/simpliSafeAlarmState.groovy
+++ smartapps/simpliSafeAlarmState.src/simpliSafeAlarmState.groovy
@@ -2,7 +2,7 @@
  *  SimpliSafe Alarm State revision 2
  *  12-25-2015
  *
- *  Copyright 2015
+ *  Copyright 2015 Ben Fox
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
